402e857225842dcc61faba47afe07c269af39c5a
João Pedro Nogueira <jpedronogueira.dev@gmail.com>
2023-01-24 19:09:14 -0400

    Tokens configurados

---
+++ packages/tokens/src/index.ts
@@ -0,0 +1,9 @@
+export * from './colors'
+
+export * from './radii'
+export * from './space'
+
+export * from './fonts'
+export * from './font-sizes'
+export * from './font-weights'
+export * from './line-heights'
