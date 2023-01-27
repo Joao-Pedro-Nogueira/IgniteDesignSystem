0ddd8fc792f40e5ab60d5e7c5d8ce4c31ce066b8
João Pedro Nogueira <jpedronogueira.dev@gmail.com>
2023-01-27 16:42:52 -0400

    Stitches configurado

---
+++ packages/react/src/index.tsx
@@ -0,0 +1,12 @@
+import { styled } from './styles'
+
+const Button = styled('button', {
+  fontFamily: '$default',
+  backgroundColor: '$ignite500',
+  borderRadius: '$md',
+  padding: '$4',
+})
+
+export function App() {
+  return <Button>Hello Ignite</Button>
+}
