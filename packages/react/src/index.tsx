bf993e0ebc9ee9e23f6766b6bc5ba6b534fd8776
João Pedro Nogueira <jpedronogueira.dev@gmail.com>
2023-01-31 19:53:29 -0400

    Primeiro componente no Storybook + dark theme

--- packages/react/src/index.tsx
+++ packages/react/src/index.tsx
@@ -1,12 +1,31 @@
+import { ComponentProps } from 'react'
+
 import { styled } from './styles'
 
-const Button = styled('button', {
+export const Button = styled('button', {
   fontFamily: '$default',
-  backgroundColor: '$ignite500',
+  backgroundColor: '$ignite300',
   borderRadius: '$md',
-  padding: '$4',
+  border: 0,
+  fontWeight: '$bold',
+  color: '$white',
+
+  variants: {
+    size: {
+      small: {
+        fontSize: 14,
+        padding: '$2 $4',
+      },
+      large: {
+        fontSize: 16,
+        padding: '$3 $6',
+      },
+    },
+  },
+
+  defaultVariants: {
+    size: 'small',
+  },
 })
 
-export function App() {
-  return <Button>Hello Ignite</Button>
-}
+export type ButtonProps = ComponentProps<typeof Button>
